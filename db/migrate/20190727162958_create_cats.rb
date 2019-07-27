e248f18f1e13b876440ba381e7da4c1c7e9afa92
Jonathan Wong <jonathan.wong110@gmail.com>
2019-07-27 16:30:11 +0000

    Automatically backed up by Learn

--- db/migrate/20190727162958_create_cats.rb
+++ db/migrate/20190727162958_create_cats.rb
@@ -1,4 +1,9 @@
-class CreateCats < ActiveRecord::Migration
+class CreateCats < ActiveRecord::Base
   def change
+    create_table :cats do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
+    end
   end
 end
